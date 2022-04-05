9f8cd3c2370bfd451b908eb7f8d2415f8462c8da
Luis Alcantara <emilioag@Luiss-MacBook-Pro-4.local>
2022-04-04 21:49:21 -0500

    menu

--- src/App.js
+++ src/App.js
@@ -18,7 +18,9 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
-
+import AvatarStack from './Components/AvatarStack'
+import StandardImageList from './Components/StandardImageList'
+import BasicGrid from './Components/BasicGrid'
 const drawerWidth = 240;
 
 const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
@@ -80,6 +82,7 @@ export default function App() {
 
   return (
     <Box sx={{ display: 'flex' }}>
+      <Router>
       <CssBaseline />
       <AppBar position="fixed" open={open}>
         <Toolbar>
@@ -117,49 +120,27 @@ export default function App() {
         </DrawerHeader>
         <Divider />
         <List>
-          <Router>
-          {['Inbox', 'Starred', 'Sendemail', 'Drafts'].map((text, index) => (
+          
+          {['Avatar', 'Grid', 'Image'].map((text, index) => (
             <ListItem button key={text}>
               <ListItemIcon>
                 {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
               </ListItemIcon>
               <Link to={text}>{text}</Link>
             </ListItem>
-          ))}
-          </Router>
+          ))}   
         </List>
         
       </Drawer>
       <Main open={open}>
         <DrawerHeader />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-          enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-          imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-          Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-          Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-          nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-          leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-          feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-          consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-          sapien faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-          eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-          neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-          tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-          sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-          tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-          gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-          et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-          tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
+          <Routes>
+            <Route exact path="/Avatar" element={<AvatarStack />}/>
+            <Route exact path="/Grid" element={<BasicGrid />}/>
+            <Route exact path="/Image" element={<StandardImageList />}/>
+          </Routes>
       </Main>
+      </Router>
     </Box>
   );
 }
